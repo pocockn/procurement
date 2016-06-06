b65b26d0ae809ec43fcf529abeed6e1888d2ba6b
Nick Pocock <nick.pocock@informa.com>
2016-06-06 14:18:40 +0100

    added ratpack serverConfig to start the process of getting connected to our db

--- src/main/groovy/persistence/DatabaseConfig.groovy
+++ src/main/groovy/persistence/DatabaseConfig.groovy
@@ -1,18 +1,13 @@
 package persistence
-
-import groovy.sql.Sql
-
 /**
  * Created by pocockn on 06/06/16.
  */
 class DatabaseConfig {
 
-    String dbUrl
-    String dbUser
+    String dbHost = "localhost"
+    String db = "surgery"
+    String dbUser = "pocockn"
     String dbPassword
     String dbDriver = "org.postgresql.Driver"
 
-    public databaseConnection() {
-        return Sql.newInstance(this.dbUrl,this.dbUser,this.dbPassword,this.dbDriver)
-    }
 }
--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,24 +1,34 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import persistence.DatabaseConfig
 import ratpack.groovy.template.MarkupTemplateModule
 
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
-import persistence.DatabaseConfig
-
 
 ratpack {
-  bindings {
-    module MarkupTemplateModule
-  }
-  serverConfig {
-    json "dbconfig.json"
-    require("/database", DatabaseConfig)
-  }
-
-  handlers {
-    get {
-      render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
+    bindings {
+        module MarkupTemplateModule
     }
 
-    files { dir "public" }
-  }
+    /* serverConfig block - specify sources of configuration and map the config into our config model */
+    serverConfig {
+        // specify the source of the configuration
+        json "dbconfig.json"
+        // Map the /database configuration path to our model object
+        // Filled by the config system, the DatabaseConfig class will become usable throughout our application
+        require("/database", DatabaseConfig)
+    }
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    handlers {
+        get("config") {
+            DatabaseConfig config -> render mapper.writeValueAsString(config)
+        }
+        get {
+            render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
+        }
+
+        files { dir "public" }
+    }
 }
